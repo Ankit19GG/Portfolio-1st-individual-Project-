ac1bcec7875f034151552fb56870d11ae819349a
Ankit Karak <ankitkarak19@gmail.com>
2025-03-24 17:20:06 +0530

    commit 8

---
+++ Portfolio.js
@@ -0,0 +1,12 @@
+function abtmeview(){
+    document.getElementById("abtview").scrollIntoView({behavior:"smooth"})
+}
+function myexpview(){
+    document.getElementById("expview").scrollIntoView({behavior:"smooth"})
+}
+function mypjtview(){
+    document.getElementById("pjtview").scrollIntoView({behavior:"smooth"})
+}
+function cntmeview(){
+    document.getElementById("cntview").scrollIntoView({behavior:"smooth"})
+}
